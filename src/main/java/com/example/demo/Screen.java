9b4a00fa05f289918e628701f1d1218a20b1569d
batzcoding <144021993+batzcoding@users.noreply.github.com>
2023-11-24 10:36:04 +0100

    lavet main metode

---
+++ src/main/java/com/example/demo/Screen.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import javafx.application.Application;
+
+public class Screen extends Application {
+    @Override
+    public static void main(String[] args){
+
+
+    }
+
+}
