228a9874d84d63eb1c85cd31eb6800958b5979f7
Bondjono19 <144017805+Bondjono19@users.noreply.github.com>
2023-11-28 16:12:19 +0100

    domain mappe

---
+++ src/main/java/com/example/demo/domain/Inventory.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain;
+import java.util.ArrayList;
+
+public class Inventory {
+    
+    private ArrayList<Item> inventory;
+
+    public 
+
+}
---
+++ src/main/java/com/example/demo/domain/Item.java
@@ -0,0 +1,14 @@
+package com.example.demo.domain;
+
+public class Item {
+    
+    Item(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    private String name;
+
+    private double price;
+
+}
