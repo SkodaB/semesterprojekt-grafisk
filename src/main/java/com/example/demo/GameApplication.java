19fea929db5d9aa3baf3cfb81a90a4e5b69a2d80
Bondjono19 <144017805+Bondjono19@users.noreply.github.com>
2023-11-28 14:37:01 +0100

    roomSwitch

--- src/main/java/com/example/demo/GameApplication.java
+++ src/main/java/com/example/demo/GameApplication.java
@@ -9,18 +9,31 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class GameApplication extends Application {
+
+    public static Stage theStage;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("World Of water!");
-        stage.setScene(scene);
+        theStage = stage;
+        //FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("hello-view.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //stage.setTitle("World Of water!");
+        //stage.setScene(scene);
+        loadScene("hello-view.fxml");
         //Image pic1 = new Image("src\\main\\resources\\com\\example\\demo\\ENTRY PIC.png");
         //stage.getIcons().add(pic1);
-        stage.show();
+        //stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static void loadScene(String fxml) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource(fxml));
+        Scene scene = new Scene(fxmlLoader.load(),320,240);
+        theStage.setTitle("Worlder of water!");
+        theStage.setScene(scene);
+        theStage.show();
     }
 }
--- src/main/java/com/example/demo/GameController.java
+++ src/main/java/com/example/demo/GameController.java
@@ -11,4 +11,21 @@ public class GameController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Eow dig!");
     }
+
+    @FXML
+    public void toEntry(){
+        try{
+            GameApplication.loadScene("hello-view.fxml");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    @FXML
+    public void toSouthCave(){
+        try{
+            GameApplication.loadScene("southcave.fxml");
+        }catch(Exception e){
+            System.out.println(e);
+        } 
+    }
 }
