f50883d5822d3f76c785c26e640fd7502811f53f
SkodaB <nicolai.skoda.bendtsen@gmail.com>
2023-11-28 13:09:15 +0100

    .

--- src/main/java/com/example/demo/GameApplication.java
+++ src/main/java/com/example/demo/GameApplication.java
@@ -8,12 +8,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("World Of water!");
         stage.setScene(scene);
         Image pic1 = new Image("/ENTRY PIC.png");
         stage.getIcons().add(pic1);
