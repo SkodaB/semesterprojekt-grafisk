afd0e433910ed421f3dc1d2af210d765d34718af
batzcoding <144021993+batzcoding@users.noreply.github.com>
2023-11-29 16:42:34 +0100

    Added South Cave Controller

--- src/main/java/com/example/demo/NorthController.java
+++ src/main/java/com/example/demo/NorthController.java
@@ -1,9 +1,10 @@
 package com.example.demo;
+
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.fxml.FXML;
 
-public class NorthController {
+public class NorthController  {
     @FXML private ImageView northImageView;
     @FXML private Image nCavePic = new Image ("/North-Cave.png");
 
