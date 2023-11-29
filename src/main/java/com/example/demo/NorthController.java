e6a3e09b7da453c045a7a6dc46b0ca7681b41ef6
batzcoding <144021993+batzcoding@users.noreply.github.com>
2023-11-29 18:05:23 +0100

    ..

--- src/main/java/com/example/demo/NorthController.java
+++ src/main/java/com/example/demo/NorthController.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.fxml.FXML;
 
 public class NorthController  {
     @FXML private ImageView northImageView;
-    @FXML private Image nCavePic = new Image ("/North-Cave.png");
+    @FXML Button myButton;
+    @FXML private Image nCavePic = new Image ("northcave.png");
+    public void displayImage(){
+        northImageView.setImage(nCavePic);
+    }
 
 
 
