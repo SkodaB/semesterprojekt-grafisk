37166173e4e6bfdadb16681b2669de891fbc8159
batzcoding <144021993+batzcoding@users.noreply.github.com>
2023-12-02 15:11:32 +0100

    bevægelse er færdig, men det får mig til at kaste op

--- src/main/java/com/example/demo/GameController.java
+++ src/main/java/com/example/demo/GameController.java
@@ -8,14 +8,14 @@ public class GameController {
     @FXML
     private Label welcomeText;
 
-    @FXML
-    public void toEntry(ActionEvent event){
-        try{
-            GameApplication.loadScene("hello-view.fxml");
-        }catch(Exception e){
-            System.out.println(e);
-        }
-    }
+//    @FXML
+//    public void toEntry(ActionEvent event){
+//        try{
+//            GameApplication.loadScene("hello-view.fxml");
+//        }catch(Exception e){
+//            System.out.println(e);
+//        }
+//    }
     @FXML
     public void toNorthCave(ActionEvent event){
         try{
@@ -32,4 +32,70 @@ public class GameController {
             System.out.println(e);
         }
     }
+    @FXML
+    public void toEntrance(ActionEvent event){
+        try{
+            GameApplication.loadScene("entry.fxml");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    @FXML
+    public void toWestCave(ActionEvent event){
+        try{
+            GameApplication.loadScene("westcave.fxml");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    @FXML
+    public void toEastCave(ActionEvent event){
+        try{
+            GameApplication.loadScene("eastcave.fxml");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    @FXML
+    public void toShop(ActionEvent event){
+        try{
+            GameApplication.loadScene("shop.fxml");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    @FXML
+    public void toWR(ActionEvent event){
+        try{
+            GameApplication.loadScene("wr.fxml");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    @FXML
+    public void toRoughFilter(ActionEvent event){
+        try{
+            GameApplication.loadScene("roughfilter.fxml");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    @FXML
+    public void toFineFilter(ActionEvent event){
+        try{
+            GameApplication.loadScene("finefilter.fxml");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    @FXML
+    public void toActiveCoal(ActionEvent event){
+        try{
+            GameApplication.loadScene("activecoal.fxml");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    //En command til at sige du kan ikke gå den her vej
+
 }
