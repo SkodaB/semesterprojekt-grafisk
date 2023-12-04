3ee84ac9b8927cf152ff63e7d9c521ee5813a9e4
batzcoding <144021993+batzcoding@users.noreply.github.com>
2023-12-04 18:26:55 +0100

    BaseCommand

---
+++ src/main/java/com/example/demo/domain/BaseCommand.java
@@ -0,0 +1,16 @@
+package com.example.demo.domain;
+
+/* Baseclass for commands
+ */
+
+class BaseCommand {
+  String description = "Undocumented";
+
+  protected boolean guardEq (String[] parameters, int bound) {
+    return parameters.length!=bound;
+  }
+
+  public String getDescription () {
+    return description;
+  }
+}
