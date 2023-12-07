d2d67d289d65c5623f658ccf7e4ac86037a929a2
batzcoding <144021993+batzcoding@users.noreply.github.com>
2023-12-07 19:03:58 +0100

    Fixed questions

--- src/main/java/com/example/demo/GameController.java
+++ src/main/java/com/example/demo/GameController.java
@@ -337,7 +337,7 @@ public class GameController implements Initializable{
     //En command til at sige du kan ikke gå den her vej
     public void wrongDirection (ActionEvent event){
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("HUR HUR HUR HUR");
+        alert.setTitle("Wrong direction :(");
         alert.setContentText("Click 'OK'");
         if (alert.showAndWait().get() == ButtonType.OK){
             alert.close();
@@ -431,7 +431,23 @@ public class GameController implements Initializable{
         waterCount.setText(""+Main.getContext().getCurrent().getWaterAmount());
     }
 
-    // Switch to questions
+    public void wrongChoice (ActionEvent event){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Nope that is not correct :(");
+        alert.setContentText("Try again by clicking 'OK'");
+        if (alert.showAndWait().get() == ButtonType.OK){
+            alert.close();
+        }
+    }
+    public void rightChoice (ActionEvent event){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Yay that is correct :)");
+        alert.setContentText("Exit the game by clicking 'OK'");
+        if (alert.showAndWait().get() == ButtonType.OK){
+            alert.close();
+            Platform.exit();
+        }
+    }
 
     public void endGame(){
         try{
@@ -439,14 +455,9 @@ public class GameController implements Initializable{
         }catch(Exception e){
             System.out.println(e);
         }
+    }
 
-    }
-    public void questionPopup() throws IOException {
-            try{GameApplication.loadScene("questions.fxml");
-            }catch (Exception e){
-                System.out.println(e);
-            }
-    }
+
     @FXML private Label qLabel;
     @FXML
     private RadioButton rb1, rb2;
