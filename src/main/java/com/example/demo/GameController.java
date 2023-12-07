070df1ce07c90aeb5b4fb093547c2063d41d1709
batzcoding <144021993+batzcoding@users.noreply.github.com>
2023-12-07 19:14:08 +0100

    Har slettet nogle ting som vi ikke bruger + ændret lidt på layout af endgame

--- src/main/java/com/example/demo/GameController.java
+++ src/main/java/com/example/demo/GameController.java
@@ -456,20 +456,4 @@ public class GameController implements Initializable{
             System.out.println(e);
         }
     }
-
-
-    @FXML private Label qLabel;
-    @FXML
-    private RadioButton rb1, rb2;
-
-    public void showQuestions(){
-        qLabel.setText("What can you do to contribute to the achievement of UNs Goal 6,'\n' with focus on the sub-goal 6.6 and 6.3?");
-        if(rb1.isSelected()){
-            //viser om hvis det er rigtig eller ej
-        }
-        else if(rb2.isSelected()){
-            //viser om hvis det er rigtig eller ej
-        }
-
-    }
 }
